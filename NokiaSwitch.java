b6f091afdfb05c1cf91f7394dd99b212b69f4fbb
samuelgbenga <samuelgbenga972@gmail.com>
2026-03-18 01:51:29 +0100

    level one

--- NokiaSwitch.java
+++ NokiaSwitch.java
@@ -5,5 +5,42 @@ public class NokiaSwitch {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        String levelOne = """
+                1  -> Phone Book
+                2  -> Messages
+                3  -> Chat
+                4  -> Call Register
+                5  -> Tones
+                6  -> Settings
+                7  -> Call Divert
+                8  -> Games
+                9  -> Calculator
+                10 -> Reminders
+                11 -> Clock
+                12 -> Profiles
+                13 -> SIM Services
+                0  -> End
+                """;
+
+        
+         String levelTwoOne = """
+                1  -> Search
+                2  -> Service Nos
+                3  -> Add Name
+                4  -> Erase
+                5  -> Edit
+                6  -> Assign tone
+                7  -> Send b'Card
+                8  -> Options
+                9  -> Speed dials
+                10 -> Voice tags
+                0  -> Back
+                """;
+
+
+                System.out.println(levelOne);
     }
+
+    
 }
